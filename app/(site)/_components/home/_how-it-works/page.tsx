7b3906b853cac8b790a1d575fbeea9d44430f851
sohan284 <sr.sohan088@gmail.com>
2025-04-12 13:02:13 +0600

    add landing

--- app/(site)/_components/home/_how-it-works/page.tsx
+++ app/(site)/_components/home/_how-it-works/page.tsx
@@ -1,52 +1,59 @@
-import Image from "next/image";
+import React from "react";
 
 export default function HowItWorks() {
+  const steps = [
+    {
+      id: 1,
+      title: "Instant Analysis",
+      description: "Fill out a quick form to see if we're a good fit.",
+    },
+    {
+      id: 2,
+      title: "Compliance Check",
+      description: "We analyze your business & craft the perfect strategy.",
+    },
+    {
+      id: 3,
+      title: "Custom Rules",
+      description:
+        "Our AI system starts running, and you watch leads turn into customers.",
+    },
+  ];
+
   return (
-    <div className="max-w-[960px] mx-auto px-4 lg:px-0 py-10 flex flex-col gap-12">
-      <h1 className="text-3xl font-semibold text-[#0F4487] text-center">
+    <div className="bg-[#212433] mx-auto w-full py-20">
+      <h1 className="text-3xl font-semibold text-white text-center">
         How It Works?
       </h1>
-      <div className="flex flex-col lg:flex-row justify-between items-start">
-        <div className="w-full p-6 flex flex-col gap-8 items-center">
-          <div className="w-full flex flex-col justify-center items-center max-w-[90px] max-h-[90px] p-[3px] border border-[#E9E9E9] rounded-lg">
-            <p className="text-[#E9E9EA] text-2xl font-medium px-[37px] py-[26px] bg-[#34A6DF] rounded-lg">1</p>
-          </div>
-          <div className="flex flex-col gap-2 items-center h-[120px]">
-            <p className="text-[#1D1F2C] text-xl font-medium text-center">
-              Instant Analysis
-            </p>
-            <p className="text-[#4A4C56] text-sm font-normal text-center">
-              Get results instantly with AI-driven analysis.
-            </p>
-          </div>
-        </div>
-        <div className="border border-dashed md:w-96 h-[200px] md:h-[2px] text-[#DBDBDB] md:mt-16 "></div>
-        <div className="w-full p-6 flex flex-col gap-8 items-center">
-          <div className="w-full flex flex-col justify-center items-center max-w-[90px] max-h-[90px] p-[3px] border border-[#E9E9E9] rounded-lg">
-            <p className="text-[#E9E9EA] text-2xl font-medium px-[37px] py-[26px] bg-[#FF914D] rounded-lg">2</p>
-          </div>
-          <div className="flex flex-col gap-2 items-center h-[120px]">
-            <p className="text-[#1D1F2C] text-xl font-medium text-center">
-              Compliance Check
-            </p>
-            <p className="text-[#4A4C56] text-sm font-normal text-center">
-              Select predefined rules to ensure product compliance with regulations and standards.
-            </p>
-          </div>
-        </div>
-        <div className="border border-dashed md:w-96 h-[200px] md:h-[2px] text-[#DBDBDB] md:mt-16 "></div>
-        <div className="w-full p-6 flex flex-col gap-8 items-center">
-          <div className="w-full flex flex-col justify-center items-center max-w-[90px] max-h-[90px] p-[3px] border border-[#E9E9E9] rounded-lg">
-            <p className="text-[#E9E9EA] text-2xl font-medium px-[37px] py-[26px] bg-[#8C52FF] rounded-lg">3</p>
-          </div>
-          <div className="flex flex-col gap-2 items-center h-[120px]">
-            <p className="text-[#1D1F2C] text-xl font-medium text-center">
-              Custom Rules
-            </p>
-            <p className="text-[#4A4C56] text-sm font-normal text-center">
-              Create custom rules tailored to their industry's unique requirements.
-            </p>
-          </div>
+      <div className="pt-14 max-w-[872px] mx-auto">
+        <div className="flex flex-col md:flex-row gap-8 mx-auto relative">
+          {steps.map((step, index) => (
+            <React.Fragment key={step.id}>
+              {/* Card */}
+              <div className="bg-white rounded-lg p-6 flex-1 relative z-10 mx-5 lg:mx-0">
+                <div
+                  className="w-11 h-11 rounded-full flex items-center justify-center mb-4 border-2 border-[#1d1f2c]"
+                  style={{
+                    background: "radial-gradient(circle, #1d1f2c, #5c5d67)",
+                  }}
+                >
+                  <span className="text-white font-medium">{step.id}</span>
+                </div>
+                <h3 className="text-[20px] font-bold mb-2">{step.title}</h3>
+                <p className="text-sm text-gray-600">{step.description}</p>
+              </div>
+
+              {/* Connecting Line */}
+              {index < steps.length - 1 && (
+                <div
+                  className="hidden md:block absolute top-[43%] w-[100px] h-[28px] bg-white"
+                  style={{
+                    left: `${27 + index * 33}%`,
+                  }}
+                ></div>
+              )}
+            </React.Fragment>
+          ))}
         </div>
       </div>
     </div>
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,15 +3,17 @@ import WhatWeDo from "./_components/home/_what-we-do/page";
 import WhyTrustScan from "./_components/home/_why-trust-scan/page";
 import HowItWorks from "./_components/home/_how-it-works/page";
 import { Suspense } from "react";
+import OptimizeReport from "./_components/home/_optimize-report/page";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen gap-20">
+    <main className="flex flex-col min-h-screen">
       <Suspense fallback={<div>Loading...</div>}>
         <Hero />
         <WhatWeDo />
         <WhyTrustScan />
         <HowItWorks />
+        <OptimizeReport />
       </Suspense>
     </main>
   );
