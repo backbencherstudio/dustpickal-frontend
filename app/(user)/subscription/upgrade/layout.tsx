096b38980298b0900395c391f8f23b2b926e23e9
sohan284 <sr.sohan088@gmail.com>
2025-04-15 15:31:20 +0600

    add payment

--- app/(user)/subscription/upgrade/layout.tsx
+++ app/(user)/subscription/upgrade/layout.tsx
@@ -1,16 +1,20 @@
-'use client';
-import { useState } from 'react';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+"use client";
+import { useState } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-export default function UpgradeLayout({ children }: { children: React.ReactNode }) {
-    const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+export default function UpgradeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const stripePromise = loadStripe(
+    "pk_test_51R38ZfC4aiT9yFQNU2nbo9gUpuJFzdx4H52vy9NZakwjjw3pXv6DMiD0jBX6ufVV93BbpKzkpQm4J3C1QtZSM1CL00wNfANGMT"
+  );
 
-    return (
-        <div>
-            <Elements stripe={stripePromise}>
-                {children}
-            </Elements>
-        </div>
-    );
+  return (
+    <div>
+      <Elements stripe={stripePromise}>{children}</Elements>
+    </div>
+  );
 }
--- app/store/api/baseApi.tsx
+++ app/store/api/baseApi.tsx
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define your base API URL
-const baseUrl = process.env.NEXT_PUBLIC_API_URL + "/api" || "http://localhost:4000/api";
+const baseUrl = "http://localhost:4000/api";
 
 export const baseApi = createApi({
   reducerPath: "api",
@@ -11,9 +11,8 @@ export const baseApi = createApi({
       const token = localStorage.getItem("token");
       headers.set(
         "authorization",
-        `Bearer ${token}`
+        `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRhbnZpcmJkY2FsbGluZ25vZGUxLmpzQGdtYWlsLmNvbSIsInN1YiI6ImNtOWY3eDhjNDAwMDJ0enV3bGFuZ2tneGUiLCJpYXQiOjE3NDQ3MDYyMTEsImV4cCI6MTc0NDc5MjYxMX0.BrauHIbw_f4Gi419eFTFxKW2nMk2c36i4qxG9TrfKMA`
       );
-
       return headers;
     },
   }),
