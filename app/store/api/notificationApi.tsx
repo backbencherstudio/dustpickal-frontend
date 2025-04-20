30fa4075bf121115e086bbf7a198b174a13fd2b0
sohan284 <sr.sohan088@gmail.com>
2025-04-20 17:28:16 +0600

    notification added

--- app/store/api/notificationApi.tsx
+++ app/store/api/notificationApi.tsx
@@ -4,23 +4,14 @@ export const notificationApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getNotifications: builder.query({
       query: (params = { page: 1, limit: 5 }) => ({
-        url: `/notifications`,
+        url: `/admin/notification`,
         method: "GET",
         params,
       }),
       providesTags: ["Notifications"],
     }),
-
-    markNotificationAsRead: builder.mutation({
-      query: (id) => ({
-        url: `/notifications/${id}/read`,
-        method: "PATCH",
-      }),
-      invalidatesTags: ["Notifications"],
-    }),
   }),
   overrideExisting: false,
 });
 
-export const { useGetNotificationsQuery, useMarkNotificationAsReadMutation } =
-  notificationApi;
+export const { useGetNotificationsQuery } = notificationApi;
